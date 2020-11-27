6d10223228319dcd2d21ac24dbc7eb4fa5abf0fb
Louis Choiniere <louischoiniere2001@gmail.com>
2020-11-26 19:10:29 -0500

    days summary report

--- src/controller/SummaryReportController.java
+++ src/controller/SummaryReportController.java
@@ -7,6 +7,7 @@ import model.Trip;
 import java.lang.reflect.Array;
 import java.net.URL;
 import java.text.DecimalFormat;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 public class SummaryReportController extends MainController {
@@ -84,6 +85,7 @@ public class SummaryReportController extends MainController {
     private double total180Days = 0;
     private double totalYearDays = 0;
 
+    private double average30Kilometer = 0;
     private double average180Kilometer = 0;
     private double averageYearKilometer = 0;
     private double average30Fuel = 0;
@@ -121,19 +123,30 @@ public class SummaryReportController extends MainController {
         List<Double> list180FUEL = new ArrayList<>();
         List<Double> listYearFUEL = new ArrayList<>();
 
-        for(Trip trip : list30)
+        List<Integer> list30DAYS = new ArrayList<>();
+        List<Integer> list180DAYS = new ArrayList<>();
+        List<Integer> listYearDAYS = new ArrayList<>();
+
+        for (Trip trip : list30)
             list30KM.add(trip.getMilesDriven());
-        for(Trip trip : list180)
+        for (Trip trip : list180)
             list180KM.add(trip.getMilesDriven());
-        for(Trip trip : listYear)
+        for (Trip trip : listYear)
             listYearKM.add(trip.getMilesDriven());
 
-        for(Trip trip : list30)
+        for (Trip trip : list30)
             list30FUEL.add(trip.getFuelConsumption() * trip.getGallonPurchased());
-        for(Trip trip : list180)
+        for (Trip trip : list180)
             list180FUEL.add(trip.getFuelConsumption() * trip.getGallonPurchased());
-        for(Trip trip : listYear)
+        for (Trip trip : listYear)
             listYearFUEL.add(trip.getFuelConsumption() * trip.getGallonPurchased());
+
+        for (Trip trip : list30)
+            list30DAYS.add((int)ChronoUnit.DAYS.between(trip.getDepartDate().toLocalDate(), trip.getReturnDate().toLocalDate()));
+        for (Trip trip : list180)
+            list180DAYS.add((int)ChronoUnit.DAYS.between(trip.getDepartDate().toLocalDate(), trip.getReturnDate().toLocalDate()));
+        for (Trip trip : listYear)
+            listYearDAYS.add((int)ChronoUnit.DAYS.between(trip.getDepartDate().toLocalDate(), trip.getReturnDate().toLocalDate()));
 
         Collections.sort(list30KM);
         Collections.sort(list180KM);
@@ -143,29 +156,36 @@ public class SummaryReportController extends MainController {
         Collections.sort(list180FUEL);
         Collections.sort(listYearFUEL);
 
-        if(list30.size() % 2 == 0){
-            median30Kilometer = ((double) list30KM.get(list30KM.size()/2) + (double) list30KM.get(list30KM.size()/2 - 1))/2;
-            median30Fuel = ((double) list30FUEL.get(list30FUEL.size()/2) + list30FUEL.get(list30FUEL.size()/2 - 1))/2;
+        Collections.sort(list30DAYS);
+        Collections.sort(list180DAYS);
+        Collections.sort(listYearDAYS);
+
+        if (list30.size() % 2 == 0) {
+            median30Kilometer = ((double) list30KM.get(list30KM.size() / 2) + (double) list30KM.get(list30KM.size() / 2 - 1)) / 2;
+            median30Fuel = ((double) list30FUEL.get(list30FUEL.size() / 2) + list30FUEL.get(list30FUEL.size() / 2 - 1)) / 2;
+            median30Days = ((double) list30DAYS.get(list30DAYS.size() / 2) + list30DAYS.get(list30DAYS.size() / 2 - 1)) / 2;
+        } else {
+            median30Kilometer = (double) list30KM.get(list30KM.size() / 2);
+            median30Fuel = list30FUEL.get(list30FUEL.size() / 2);
+            median30Days = list30DAYS.get(list30DAYS.size() / 2);
         }
-        else{
-            median30Kilometer = (double) list30KM.get(list30KM.size()/2);
-            median30Fuel = list30FUEL.get(list30FUEL.size()/2);
+        if (list180.size() % 2 == 0) {
+            median180Kilometer = ((double) list180KM.get(list180KM.size() / 2) + (double) list180KM.get(list180KM.size() / 2 - 1)) / 2;
+            median180Fuel = ((double) list180FUEL.get(list180FUEL.size() / 2) + list180FUEL.get(list180FUEL.size() / 2 - 1)) / 2;
+            median180Days = ((double) list180DAYS.get(list180DAYS.size() / 2) + list180DAYS.get(list180DAYS.size() / 2 - 1)) / 2;
+        } else {
+            median180Kilometer = (double) list180KM.get(list180KM.size() / 2);
+            median180Fuel = list180FUEL.get(list180FUEL.size() / 2);
+            median180Days = list180DAYS.get(list180DAYS.size() / 2);
         }
-        if(list180.size() % 2 == 0){
-            median180Kilometer = ((double) list180KM.get(list180KM.size()/2) + (double) list180KM.get(list180KM.size()/2 - 1))/2;
-            median180Fuel = ((double) list180FUEL.get(list180FUEL.size()/2) + list180FUEL.get(list180FUEL.size()/2 - 1))/2;
-        }
-        else{
-            median180Kilometer = (double) list180KM.get(list180KM.size()/2);
-            median180Fuel = list180FUEL.get(list180FUEL.size()/2);
-        }
-        if(listYear.size() % 2 == 0){
-            medianYearKilometer = ((double) listYearKM.get(listYearKM.size()/2) + (double) listYearKM.get(listYearKM.size()/2 - 1))/2;
-            medianYearFuel = ((double) listYearFUEL.get(listYearFUEL.size()/2) + listYearFUEL.get(listYearFUEL.size()/2 - 1))/2;
-        }
-        else{
-            medianYearKilometer = (double) listYearKM.get(listYearKM.size()/2);
-            medianYearFuel = listYearFUEL.get(listYearFUEL.size()/2);
+        if (listYear.size() % 2 == 0) {
+            medianYearKilometer = ((double) listYearKM.get(listYearKM.size() / 2) + (double) listYearKM.get(listYearKM.size() / 2 - 1)) / 2;
+            medianYearFuel = ((double) listYearFUEL.get(listYearFUEL.size() / 2) + listYearFUEL.get(listYearFUEL.size() / 2 - 1)) / 2;
+            medianYearDays = ((double) listYearDAYS.get(listYearDAYS.size() / 2) + listYearDAYS.get(listYearDAYS.size() / 2 - 1)) / 2;
+        } else {
+            medianYearKilometer = (double) listYearKM.get(listYearKM.size() / 2);
+            medianYearFuel = listYearFUEL.get(listYearFUEL.size() / 2);
+            medianYearDays = listYearDAYS.get(listYearDAYS.size() / 2);
         }
 
         // --------------------   KILOMETER SECTION   ------------------//
@@ -175,8 +195,8 @@ public class SummaryReportController extends MainController {
         totalYearKilometer = listYear.stream().mapToInt(o -> o.getMilesDriven()).sum();
 
         // AVERAGE
-        double average30Kilometer = list30.stream().mapToInt(o -> o.getMilesDriven()).average().orElse(0);
-        average180Kilometer  = list180.stream().mapToInt(o -> o.getMilesDriven()).average().orElse(0);
+        average30Kilometer = list30.stream().mapToInt(o -> o.getMilesDriven()).average().orElse(0);
+        average180Kilometer = list180.stream().mapToInt(o -> o.getMilesDriven()).average().orElse(0);
         averageYearKilometer = listYear.stream().mapToInt(o -> o.getMilesDriven()).average().orElse(0);
 
         // Medians are setup before this part.
@@ -186,16 +206,15 @@ public class SummaryReportController extends MainController {
         total180DaysKM.setText(d.format(total180Kilometer) + " km");
         total1YearKM.setText(d.format(totalYearKilometer) + " km");
 
-        average30DaysKM.setText(d.format(total30Kilometer) + " km");
-        average180DaysKM.setText(d.format(total180Kilometer) + " km");
-        average1YearKM.setText(d.format(totalYearKilometer) + " km");
+        average30DaysKM.setText(d.format(average30Kilometer) + " km");
+        average180DaysKM.setText(d.format(average180Kilometer) + " km");
+        average1YearKM.setText(d.format(averageYearKilometer) + " km");
 
         median30DayKM.setText(d.format(median30Kilometer) + " km");
         median180DayKM.setText(d.format(median180Kilometer) + " km");
         median1YearKM.setText(d.format(medianYearKilometer) + " km");
 
         // --------------------   END OF KILOMETER SECTION   ------------------//
-
 
 
         // --------------------   FUEL SECTION   ------------------//
@@ -226,7 +245,34 @@ public class SummaryReportController extends MainController {
 
         // --------------------   END OF FUEL SECTION   ------------------//
 
+
+        // --------------------   FUEL SECTION   ------------------//
+
+        // SET THE NUMBERS
+        total30Days = list30.stream().mapToInt(o -> (int) (ChronoUnit.DAYS.between(o.getDepartDate().toLocalDate(), o.getReturnDate().toLocalDate()))).sum();
+        total180Days = list180.stream().mapToInt(o -> (int) (ChronoUnit.DAYS.between(o.getDepartDate().toLocalDate(), o.getReturnDate().toLocalDate()))).sum();
+        totalYearDays = listYear.stream().mapToInt(o -> (int) (ChronoUnit.DAYS.between(o.getDepartDate().toLocalDate(), o.getReturnDate().toLocalDate()))).sum();
+
+        average30Days = list30.stream().mapToDouble(o -> (double) (ChronoUnit.DAYS.between(o.getDepartDate().toLocalDate(), o.getReturnDate().toLocalDate()))).average().orElse(0);
+        average180Days = list180.stream().mapToDouble(o -> (double) (ChronoUnit.DAYS.between(o.getDepartDate().toLocalDate(), o.getReturnDate().toLocalDate()))).average().orElse(0);
+        averageYearDays = listYear.stream().mapToDouble(o -> (double) (ChronoUnit.DAYS.between(o.getDepartDate().toLocalDate(), o.getReturnDate().toLocalDate()))).average().orElse(0);
+
+        for (Trip o : list30)
+            System.out.println(ChronoUnit.DAYS.between(o.getDepartDate().toLocalDate(), o.getReturnDate().toLocalDate()));
+
+        d = new DecimalFormat("#0.0");
+
+        // SET THE LABELS
+        total30DaysDAY.setText(d.format(total30Days) + " Days");
+        total180DaysDAY.setText(d.format(total180Days) + "Days");
+        total1YearDAY.setText(d.format(totalYearDays) + "Days");
+        average30DaysDAY.setText(d.format(average30Days) + "Days");
+        average180DaysDAY.setText(d.format(average180Days) + "Days");
+        average1YearDAY.setText(d.format(averageYearDays) + "Days");
+        median30DayDAY.setText(d.format(median30Days) + "Days");
+        median180DayDAY.setText(d.format(median180Days) + "Days");
+        median1YearDAY.setText(d.format(medianYearDays) + "Days");
+
+        // --------------------   FUEL SECTION   ------------------//
     }
-
-
 }
